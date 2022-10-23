19fb39ccc4f7aa8502576d15d5ff3b2ba8dc5724
Cesar Augusto Torres Ardila <ceatorresar@unal.edu.co>
2022-10-23 18:43:31 -0500

    Planet first traslation and rotation

---
+++ Source/Teletrix/TeletrixGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TeletrixGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ATeletrixGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ATeletrixGameMode();
+};
+
+
+
